9f6b77a89576d55e96112b6083630485b40aba59
Christopher Duke <cd.duke@gmail.com>
2016-06-27 21:22:57 -0500

    feedback

--- src/blackjack/card-sum.js
+++ src/blackjack/card-sum.js
@@ -1,3 +1,19 @@
 export default function cardSum(cardA, cardB) {
+  const a = (cardA + cardB)
+  const cardA = 1;
+  const cardB = 3; {
+    return 4;
+  }
 
+  const b = (cardX + cardY)
+  const cardX = 10;
+  const cardY = 20; {
+    return 30;
+  }
+
+  const c = (cardA + cardB)
+  const cardA = 1000;
+  const cardB = 200; {
+    return 10200;
+  }
 }
--- src/blackjack/should-split.js
+++ src/blackjack/should-split.js
@@ -1,3 +1,5 @@
 export default function shouldSplit(cardA, cardB) {
-
+  aceFive = shouldSplit('ace', 5); // undefined
 }
+
+aceAce = shouldSplit()
