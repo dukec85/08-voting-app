e6eaec2c65afe26d7e4af96c32a07b43d0a65a68
Christopher Duke <cd.duke@gmail.com>
2016-06-28 15:28:32 -0500

    select-action

--- src/blackjack/select-action.js
+++ src/blackjack/select-action.js
@@ -1,4 +1,15 @@
-  import shoudHit from 'blackjack/should-hit';
-  import shouldSplit from 'blackjack/should-split';
+import shouldSplit from 'blackjack/should-split';
+import cardSum from 'blackjack/card-sum';
+import cardValue from 'blackjack/card-value';
+import shouldHit from 'blackjack/should-hit';
 
 export default function selectAction(cardA, cardB) {
+
+    if (shouldHit(cardA, cardB)) {
+      return 'hit';
+    }
+    else if (shouldSplit(cardA, cardB)) {
+     return 'split';
+    }
+    return 'stay';
+}
