8f6e10e9ed2369e7c8d6a2b0cdd7b5e4a7ed83c3
Christopher Duke <cd.duke@gmail.com>
2016-06-29 17:37:30 -0500

    option-increment

--- src/voting/increment-vote.js
+++ src/voting/increment-vote.js
@@ -1,8 +1,19 @@
-export default function addOption(existingPoll, optionIndex) {
-  const collector = [];
+export default function incrementVote(existingPoll, optionIndex) {
+  let collector = [];
+  var newObject = {};
 
   for (let i = 0; i < existingPoll.length; i++) {
+
+    newObject = {
+      name: `${existingPoll[i].name}`,
+      votes: existingPoll[i].votes
+    };
+    collector.push(newObject);
+
   }
 
+  collector[optionIndex].votes++;
+
   return collector;
+
 }
